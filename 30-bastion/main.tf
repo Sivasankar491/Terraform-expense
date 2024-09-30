6006ca02458fc74fbcc774cd18dc593da5fff963
Sankar491 <sivasankarrcks@gmail.com>
2024-09-30 23:37:27 +0530

    creating bastion server

---
+++ 30-bastion/main.tf
@@ -0,0 +1,19 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = local.resource_name
+  ami = data.aws_ami.devops.id
+  
+  instance_type          = "t3.micro"
+#   key_name               = "user1"
+#   monitoring             = true
+  vpc_security_group_ids = local.bastian_sg_id
+  subnet_id              = local.public_subnet_id
+
+  tags = merge(
+    var.common_tags,
+    {
+        Name= "${local.resource_name}"
+    }
+  )
+}
