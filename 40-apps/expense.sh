d1a51fd23af72132d6e471c714a1898b169cb934
Sankar491 <sivasankarrcks@gmail.com>
2024-10-04 17:08:39 +0530

    modified expense.sh

--- 40-apps/expense.sh
+++ 40-apps/expense.sh
@@ -3,6 +3,6 @@ dnf install ansible -y
 cd /tmp/
 git clone https://github.com/Sivasankar491/expense-ansible.git
 cd expense-ansible
-ansible-plabook -i inventory.ini mysql.yaml
-ansible-plabook -i inventory.ini backend.yaml
-ansible-plabook -i inventory.ini frontend.yaml
+ansible-playbook -i inventory.ini mysql.yaml
+ansible-playbook -i inventory.ini backend.yaml
+ansible-playbook -i inventory.ini frontend.yaml
